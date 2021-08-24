376e522292c79768c5d8833da1192e94c84ce6a0
parhamjf <parham.firou@gmail.com>
2021-08-24 23:48:58 +0200

    Update config.js

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@ module.exports = Object.freeze({
   web3Rpc: process.env.WEB3_RPC || 'http://localhost:8545',
   nodePrivateKey: process.env.WEB3_NODE_PRIVATEKEY || '',
   nodeAddressKey: process.env.WEB3_NODE_ADDRESS_KEY || '',
-  bundleContractAddress: process.env.CONTRACT_BUNDLE_ADDRESS || '0xbEBFe210A4F19671Eac7587b72a24bA3C5674923',
-  bundlePath: process.env.ASSET_AND_EVENT_PATH || "../../build/pool",
+  PoolContractAddress: process.env.CONTRACT_BUNDLE_ADDRESS || '0xbEBFe210A4F19671Eac7587b72a24bA3C5674923',
+  PoolPath: process.env.ASSET_AND_EVENT_PATH || "../../build/pool",
   GasFee: process.env.GAS_FEE || "400000"
 });
